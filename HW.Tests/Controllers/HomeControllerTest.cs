ca81249c51777ec67bce61fe9021eb64ba131eef
jvenku1 <jvenku@gmail.com>
2017-03-27 15:31:53 -0400

    Hellow World Application
    Hellow World Application

---
+++ HW.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW;
+using HW.Controllers;
+
+namespace HW.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            string result = controller.Index() as string;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Hello World!", result);
+        }
+    }
+}
---
+++ HW/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HW.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
